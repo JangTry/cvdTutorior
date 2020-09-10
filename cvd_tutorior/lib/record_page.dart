085e5fa168f4d81112ff093e311d162a7302be18
JangTry <lofi0boy@gmail.com>
2020-09-11 00:09:48 +0900

    [recorder] flutter_audio_recorder로 결정 녹음 재생 러프하게 구현

--- cvd_tutorior/lib/record_page.dart
+++ cvd_tutorior/lib/record_page.dart
@@ -1,23 +1,183 @@
+import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter_sound/flutter_sound.dart';
+import 'dart:io' as io;
+
+import 'package:flutter_audio_recorder/flutter_audio_recorder.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:path_provider/path_provider.dart';
 
 class RecordPage extends StatefulWidget {
   @override
   _RecordPageState createState() => _RecordPageState();
 }
 
-class _RecordPageState extends State<RecordPage> {//record브랜치 오 여기서도 커밋이 되네? 이제 진짜 recorder 브랜치
-  FlutterSoundRecorder soundRecorder = new FlutterSoundRecorder();
-  FlutterSoundPlayer soundPlayer = new FlutterSoundPlayer();
+class _RecordPageState extends State<RecordPage> {
+  //record브랜치 오 여기서도 커밋이 되네? 이제 진짜 recorder 브랜치
+
+  FlutterAudioRecorder _recorder;
+  AudioPlayer player = AudioPlayer();
+  AudioPlayerState playerState = AudioPlayerState.STOPPED;
+  Recording _recording;
+  Timer _t;
+  IconData _buttonIcon = Icons.do_not_disturb_on;
+  String _alert;
 
   bool _isAnswer = true;
-  bool _onRecording = false;
   bool _isAnswerEmpty = true;
   bool _isFreeEmpty = true;
 
   @override
-  Widget build(BuildContext context) {
+  void initState() {
+    super.initState();
+    Future.microtask(() {
+      _prepare();
+    });
+  }
 
+  void _opt() async {
+
+    switch (_recording.status) {
+      case RecordingStatus.Initialized:
+        {
+          await _startRecording();
+          break;
+        }
+      case RecordingStatus.Recording:
+        {
+          await _stopRecording();
+          break;
+        }
+      case RecordingStatus.Stopped:
+        {
+          if (player.state == AudioPlayerState.PLAYING) {
+            await player.stop();
+          } else {
+            await _play();
+
+          }
+          break;
+        }
+
+      default:
+        break;
+    }
+
+    // 刷新按钮
+    setState(() {
+      _buttonIcon = _playerIcon(_recording.status,playerState);
+    });
+  }
+
+  Future _init() async {
+    String customPath = '/flutter_audio_recorder_';
+    io.Directory appDocDirectory;
+    if (io.Platform.isIOS) {
+      appDocDirectory = await getApplicationDocumentsDirectory();
+    } else {
+      appDocDirectory = await getExternalStorageDirectory();
+    }
+
+    // can add extension like ".mp4" ".wav" ".m4a" ".aac"
+    customPath = appDocDirectory.path +
+        customPath +
+        DateTime.now().millisecondsSinceEpoch.toString();
+
+    // .wav <---> AudioFormat.WAV
+    // .mp4 .m4a .aac <---> AudioFormat.AAC
+    // AudioFormat is optional, if given value, will overwrite path extension when there is conflicts.
+
+    _recorder = FlutterAudioRecorder(customPath,
+        audioFormat: AudioFormat.WAV, sampleRate: 22050);
+    await _recorder.initialized;
+  }
+
+  Future _prepare() async {
+    var hasPermission = await FlutterAudioRecorder.hasPermissions;
+    if (hasPermission) {
+      await _init();
+      var result = await _recorder.current();
+      setState(() {
+        _recording = result;
+        _buttonIcon = _playerIcon(_recording.status,playerState);
+        _alert = "";
+      });
+    } else {
+      setState(() {
+        _alert = "Permission Required.";
+      });
+    }
+  }
+
+  Future _startRecording() async {
+    await _recorder.start();
+    var current = await _recorder.current();
+    setState(() {
+      _recording = current;
+    });
+
+    _t = Timer.periodic(Duration(milliseconds: 10), (Timer t) async {
+      var current = await _recorder.current();
+      setState(() {
+        _recording = current;
+        _t = t;
+      });
+    });
+  }
+
+  Future _stopRecording() async {
+    var result = await _recorder.stop();
+    _t.cancel();
+
+    setState(() {
+      _recording = result;
+    });
+  }
+
+  void _play() {
+//    var result = player.play(_recording.path, isLocal: true);
+    var result = player.play("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3", isLocal: false);
+    if(result == 1 ) {
+      setState(() {
+        playerState = AudioPlayerState.PLAYING;
+        print('${player
+            .state} 여기야!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+      });
+    }
+  }
+
+  IconData _playerIcon(RecordingStatus recordStatus, AudioPlayerState playerState) {
+    switch (recordStatus) {
+      case RecordingStatus.Initialized:
+        {
+          return Icons.mic;
+        }
+      case RecordingStatus.Recording:
+        {
+          return Icons.stop;
+        }
+      case RecordingStatus.Stopped:
+        {
+          return checkPlayerState(playerState);
+        }
+      default:
+        return Icons.mic;
+    }
+  }
+
+  IconData checkPlayerState(AudioPlayerState playerState) {
+    print("HI!!!!!!!!!!!!!!!!!!!");
+    if (playerState == AudioPlayerState.PLAYING) {
+      print("재생중니ㅣ까 멈춰로 바꿔주자ㅓ");
+      return Icons.stop;
+    }else {
+      print("조까!!!!!!!!!");
+      return Icons.play_arrow;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return SafeArea(
       child: Container(
         padding: EdgeInsets.all(30),
@@ -217,12 +377,7 @@ class _RecordPageState extends State<RecordPage> {//record브랜치 오 여기�
                     Center(
                       child: MaterialButton(
                         splashColor: Color(0xffffacac),
-                        animationDuration: Duration(seconds: 2),
-                        onPressed: () {
-                          setState(() {
-                            _onRecording = !_onRecording;
-                          });
-                        },
+                        onPressed: _opt,
                         color: Colors.white,
                         child: Container(
                           width: 160,
@@ -231,7 +386,7 @@ class _RecordPageState extends State<RecordPage> {//record브랜치 오 여기�
                             child: FittedBox(
                                 fit: BoxFit.scaleDown,
                                 child: Icon(
-                                  _onRecording ? Icons.stop : Icons.mic,
+                                  _buttonIcon,
                                   color: Color(0xffffacac),
                                   size: 80,
                                 )),
@@ -246,14 +401,29 @@ class _RecordPageState extends State<RecordPage> {//record브랜치 오 여기�
               ),
             ),
             SizedBox(
-              height: 100,
-//              child: FittedBox(
-//
-//              ),
+              height: 10,
             ),
-            ((_isAnswer && !_isAnswerEmpty) || (!_isAnswer && !_isFreeEmpty))
+            SizedBox(
+              height: 50,
+              child: FittedBox(
+                  fit: BoxFit.scaleDown,
+                  child: Text(
+                    '${_recording?.duration.toString().substring(2, 7) ?? " "}',
+                    style: TextStyle(color: Colors.grey.withOpacity(0.5),fontSize: 50),
+                  )),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            ((_isAnswer && _recording.status == RecordingStatus.Stopped) ||
+                    (!_isAnswer && !_isFreeEmpty))
                 ? GestureDetector(
-                    onTap: () {},
+                    onTap: () {
+//                      _buttonIcon = _playerIcon();
+                      setState(() {
+                        _prepare();
+                      });
+                    },
                     child: Card(
                       shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(10)),
@@ -275,6 +445,7 @@ class _RecordPageState extends State<RecordPage> {//record브랜치 오 여기�
                     ),
                   )
                 : Card(
+                    color: Colors.transparent,
                     child: SizedBox(
                       height: 60,
                     ),
@@ -285,6 +456,7 @@ class _RecordPageState extends State<RecordPage> {//record브랜치 오 여기�
             ),
             GestureDetector(
               onTap: () {
+                print("${player.state} ㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜ");
                 setState(() {
                   _isAnswerEmpty = !_isAnswerEmpty;
                 });
@@ -314,3 +486,10 @@ class _RecordPageState extends State<RecordPage> {//record브랜치 오 여기�
     );
   }
 }
+
+
+//Todo
+//시작할때 7오류 왜나는지
+//재생주일때 정지로 바뀌기
+//답변/자유로 와리가리하기
+//다시녹음하면 파일 지우기
